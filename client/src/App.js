c7d1338920e767024b29759e25f3bf3301458098
Sondes Chemingui <sondes84chmingui@gmail.com>
2021-02-11 18:52:01 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Spinner } from 'reactstrap';
+
+import { getAuthUser } from './js/actions/authActions';
+
+import AppNavbar from './components/AppNavBar';
+import Home from './components/pages/Home';
+import PrivateRoute from './components/routess/PrivateRoute';
+import Dashboard from './components/pages/Dashboard';
+import ProfileModal from './components/auth/ProfileModal';
+
+function App() {
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.authReducer);
+  const getUser = () => dispatch(getAuthUser());
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <Spinner
+          style={{ width: '3rem', height: '3rem', color: 'secondary' }}
+          type="grow"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <AppNavbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+      
+       
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/auth/ProfileModal.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  NavLink,
+} from 'reactstrap';
+
+import { profileUser } from '../../js/actions/authActions';
+
+const ProfileModal = () => {
+  const [modal, setModal] = useState(false);
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleProfile = () => {
+    const newUser = { name, lastName, birthDate };
+    dispatch(profileUser(newUser));
+    history.push('/dashboard');
+    setName('');
+    setLastName('');
+    setBirthDate('');
+     };
+
+  return (
+    <div style={{ padding: '0 15px' }}>
+      <NavLink onClick={toggle} href="#">
+        Profile
+      </NavLink>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Profile</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input
+                type="text"
+                value={name}
+                name="name"
+                id="name"
+                placeholder="Name"
+                className="mb-3"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Label for="name">Last Name</Label>
+              <Input
+                type="text"
+                value={lastName}
+                name="lastName"
+                id="lastName"
+                placeholder="Last Name"
+                className="mb-3"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              <Label for="date">birthDate</Label>
+              <Input
+                type="text"
+                value={birthDate}
+                name="BirthDate"
+                id="birthDate"
+                placeholder="birthDate"
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
+              <Button
+                color="dark"
+                style={{ marginTop: '2rem' }}
+                block
+                onClick={handleProfile}
+              >
+               Profile
+              </Button>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+};
+
+export default ProfileModal;
